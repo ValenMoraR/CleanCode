8806ce889d0b7efc680f1096f95b67cfba5de654
Valentina Morales <111003190+ValenMoraR@users.noreply.github.com>
2024-03-22 12:15:19 -0500

    Add files via upload

---
+++ ProyectoAula2/PA2/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA2
+{
+    internal class Cliente
+    {
+        public string Cedula { get; set; }
+        public int Estrato { get; set; }
+        public int MetaAhorro { get; set; }
+        public int ConsumoActualEnergia { get; set; }
+        public int ConsumoActualAgua { get; set; }
+    }
+}
